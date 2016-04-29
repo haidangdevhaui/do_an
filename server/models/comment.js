5cc7adf8a5580ecb778aba39a955cee9db2eef42
haidangdevhaui <haidangdevhaui@gmail.com>
2016-04-29 11:38:47 +0700

    add bao_cao

--- server/models/comment.js
+++ server/models/comment.js
@@ -1,10 +1,12 @@
 var mongoose = require('mongoose');
 var model = new mongoose.Schema({
-	type: String,
-	name: String,
+	pid: String,
+	uid: String,
+	content: String,
+	status: Number,
 	crt: {
         type: Date,
         default: Date.now()
     }
 });
-module.exports = mongoose.model('examps', model);
+module.exports = mongoose.model('Comments', model);
